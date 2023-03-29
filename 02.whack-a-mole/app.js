e8fdaa0855a319886d7f1f2440a40002548f63d5
AdamGiszter <116649056+Giszta@users.noreply.github.com>
2023-03-29 14:14:46 +0200

    comments

--- 02.whack-a-mole/app.js
+++ 02.whack-a-mole/app.js
@@ -8,43 +8,46 @@ let hitPosition;
 let currentTime = 60;
 let timerId = null;
 
-
+//mole generate
 function randomSquare() {
-    squares.forEach(square => {
-        square.classList.remove('mole')
+    squares.forEach(square => {                                    
+        square.classList.remove('mole')                            // usuń 'mole' dla każdego elementu z listy clasy .square
     });
 
-    let randomSquare = squares[Math.floor(Math.random() * 9)];
-    randomSquare.classList.add('mole');
+    let randomSquare = squares[Math.floor(Math.random() * 9)];     //generowanie losowego indexu 0-8
+    randomSquare.classList.add('mole');                            //dodanie 'mole' do elementu z lisy clasy .square
 
-    hitPosition = randomSquare.id;
+    hitPosition = randomSquare.id;                                 // zmienna równa ID wylosowanego pola
 }
 
-squares.forEach(square => {
-    square.addEventListener('mousedown', () => {
-        if(square.id === hitPosition){
-            result++;
-            score.textContent = result;
-            hitPosition = null;
+//points counter
+squares.forEach(square => {                                        //dla każdej pozycji .square
+    square.addEventListener('mousedown', () => {                   //dodać nasłuchiwanie na kliknięcie. 'click' - klik i puszczenie; 'mousedown'-klik.
+        if(square.id === hitPosition){                             //jeśli ID pola jest równe polu na którym jest mole
+            result++;                                              //incrementuj result
+            score.textContent = result;                            //zmień text w #score na result
+            hitPosition = null;                                    //nie ma już dostępnego hitPosition - zapobieganie nabijanu pkt poprzez klikanie kilka razy 
         }
     });
 });
 
+//timer for moving the mole
 function moveMole() {
-    timerId = setInterval(randomSquare, 1000)
+    timerId = setInterval(randomSquare, 1000)                      //po jakim czasie następuje ponowne wywołanie funkcji randomSquare
 };
 
-moveMole();
+moveMole();                                                        //wywołanie moveMole
 
+//Timer countdown
 function countDown() {
-    currentTime--;
-    timeLeft.textContent = currentTime;
+    currentTime--;                                                 //dekrementacja czasu
+    timeLeft.textContent = currentTime;                            //nadpisanie czasu w html
 
-    if(currentTime === 0){
-        clearInterval(countDownTimerId);
-        clearInterval(timerId);
-        alert('GAME OVER! Your final score is: ' + result);
+    if(currentTime === 0){                                         //jeśli czas jest równy 0
+        clearInterval(countDownTimerId);                           //metoda usuwania timera dla countDownTimerId
+        clearInterval(timerId);                                     //metoda usuwania timera, która stopuje timerId 
+        alert('GAME OVER! Your final score is: ' + result);         //wywołanie okna alertu
     }
 };
 
-let countDownTimerId = setInterval(countDown, 1000);
+let countDownTimerId = setInterval(countDown, 1000);                //wywołanie funkcji countDown co 1s
